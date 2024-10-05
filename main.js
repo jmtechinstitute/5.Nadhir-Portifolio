55bc5d1cecd538529bbe9bbc2e1c770e7ad3999a
Mohamed Yazeen <166999856+jmtechinstitute@users.noreply.github.com>
2024-10-05 21:08:04 +0530

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed(".text", {
-    strings : ["JM TECH Mentor","BCA Graduate","Full Stack Developer"],
-    typeSpeed:30,
-    backSpeed:50,
-    backdelay:1000,
-    loop:true
-})
